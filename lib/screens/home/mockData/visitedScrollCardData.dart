e565e27f53d69906e16cf6a7a6055bcad27a2cc0
Fatima Hasan <fatimahasan@Fatimas-MacBook-Air.local>
2024-03-17 15:21:45 +0530

    latest

--- lib/screens/home/mockData/visitedScrollCardData.dart
+++ lib/screens/home/mockData/visitedScrollCardData.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:vitti_heritage_app/screens/home/models/visitedScrollCardModel.dart';
 
 final List<VisitedScrollCardModel> visitedScrollCardList = [
+  VisitedScrollCardModel(name: "Golden Temple", image: AssetImage("assets/images/onboarding3.png"), location: "Amritsar")
+];
