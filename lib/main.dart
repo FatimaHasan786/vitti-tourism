f51d9ba8a66b9967e2604d5dd72232f31d028509
FatimaHasan786 <sayadfatimahasan786@gmail.com>
2024-03-20 14:40:16 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:vitti_heritage_app/navgation/tabBarNavigation.dart';
-import 'package:vitti_heritage_app/screens/login/login.dart';
-import 'package:vitti_heritage_app/screens/login/phone.dart';
 import 'package:vitti_heritage_app/screens/onboarding/onboarding.dart';
 
 
@@ -18,7 +15,7 @@ class MyApp extends StatelessWidget {
     return SafeArea(
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Login(),
+        home: Onboarding(),
         
       ),
     );
